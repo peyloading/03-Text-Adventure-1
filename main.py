a1b2ffd193f10e763ef96493bda4acb988c18644
Gentry <watsonra@iu.edu>
2019-09-11 18:36:35 -0400

    starts project

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ assert sys.version_info >= version, "This script requires at least Python {0}.{1
 logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def render():
+def render(game,current):
     '''display the current location'''
+    r = game['rooms']
+    c = r[current]
+
+    print('\n\nYou are in the {name}'.format(name=c['name']))
+    print(c['desc'])
     return True
 
-def update():
+def update(response, game, current):
     '''update location if possible'''
-    return True
+    for e in game['rooms'][current]['exits']:
+        if e ['verb'] == response:
+            current = e['target']
+    return current
 
-def check():
+def checkInput():
     '''get user input'''
-    response = input('What would you like to do?')
-
+    response = input('What would you like to do?') .upper() .strip()
     return response
 
 
@@ -29,18 +36,14 @@ def main():
         game = json.load(json_file)
     # Your game goes here!
 
-    current = 'WHOUS'
+    current = 'LOC2'
 
-    quit = false
+    quit = False
     while not quit:
-        
-        #render
-        #check  player input
-        #update
-        
-    
-
-    return True
+        render(game,current)
+        response = checkInput()
+        current = update(response, game, current)
+        return True
 
 
 
